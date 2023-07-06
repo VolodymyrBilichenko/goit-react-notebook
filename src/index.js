36a848d49b83d65b78211ed2e62b4b1e95e73127
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-07-06 17:44:23 +0300

    Update index.js
    start react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
+// import { render } from 'react-dom';
+import ReactDOM from "react-dom";
 
+const container = document.getElementById('root');
+
+// Вступ --------------------
+
+// const elem1 = React.createElement('span', { children: 'Hello' });
+// const elem2 = React.createElement('span', { children: 'world' });
+
+
+// const elements = React.createElement('div', {
+//   a: 5,
+//   b: 10,
+//   children: [elem1, ' ', elem2],
+// });
+
+// console.log(elements);
+
+// render(elements, container);
+
+
+// JSX -------------------------
+
+
+// const elem1 = <span>Hello</span>
+// const elem2 = <span>world</span>
+
+// const elements = React.createElement('div', {
+// a: 5,
+// b: 10,
+// children: 'Hello world',
+// });
+
+// const jsxElements = <div>
+//   {elem1}
+//   {elem2}
+// </div>
+
+// console.log(jsxElements);
+
+// ReactDOM.render(jsxElements, container);
