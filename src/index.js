a4ef9236c9d986a9e506b62cca2dc853659759a5
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-07-06 18:16:08 +0300

    Update index.js
    import user.json

--- src/index.js
+++ src/index.js
@@ -59,17 +59,7 @@ const container = document.getElementById('root');
 
 
 
-const data = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  }
-};
+const data = user;
 
 const profile = (
   <div className="description">
